c4d619088c0cea34ac1f0d2c0d2dd22e104e0680
Ian Judson <i.w.judson@gmail.com>
2017-08-04 21:42:55 -0700

    clean start for project

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,25 +1,2 @@
 import React from "react";
 import { render } from "react-dom";
-
-const MyTitle = function(props) {
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>
-        {props.title}
-      </h1>
-    </div>
-  );
-};
-
-const MyFirstComponent = function() {
-  return (
-    <div id="my-first-component">
-      <MyTitle title="GoT" color="YellowGreen" />
-      <MyTitle title="Stranger Things" color="DarkBlue" />
-      <MyTitle title="Rick and Morty" color="Peru" />
-      <MyTitle title="The OA" color="Violet" />
-    </div>
-  );
-};
-
-render(<MyFirstComponent />, document.getElementById("app"));
